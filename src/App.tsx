eb6dc55138532f0533e819f24b0b8acc6a514130
Olivia <oliviatmanalastas@gmail.com>
2023-01-30 13:28:48 -0500

    using hooks in react/ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 import ShoppingList from './components/ShoppingList';
 import Greeter from './components/Greeter';
+import Item from './models/item'
+
+// interface Item {
+//   id: number;
+//   product: string;
+//   quantity: number;
+// }
 
 function App() {
-     const items = [
-       {
-         id: 1,
-         product: "Lemon",
-         quantity: 3,
-       },
-       {
-         id: 2,
-         product: "Chicken Breast",
-         quantity: 2,
-       },
-     ];
-     
+   const [items, setItems] = useState<Item[]>([])
+
+    //  const items = [
+    //    {
+    //      id: 1,
+    //      product: "Lemon",
+    //      quantity: 3,
+    //    },
+    //    {
+    //      id: 2,
+    //      product: "Chicken Breast",
+    //      quantity: 2,
+    //    },
+    //  ];
+
   return (
     <div className="App">
       <ShoppingList items={items}/>
--- src/components/ShoppingList.tsx
+++ src/components/ShoppingList.tsx
@@ -1,19 +1,15 @@
 import React from "react";
+import Item from "../models/item";
 
-interface Item {
-   id: number;
-   product: string;
-   quantity: number
-}
 interface ShoppingListProps {
    items: Item[]
 }
-function ShoppingList(props: ShoppingListProps): JSX.Element {
+function ShoppingList({items}: ShoppingListProps): JSX.Element {
   return (
     <div>
       <h1> Shopping List</h1>
       <ul>
-        {props.items.map(item => (
+        {items.map(item => (
          <li key={item.id}>{item.product} - {item.quantity}</li>
         ))}
       </ul>
