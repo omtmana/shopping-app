4a26f0515a1e740148a0a356a4220cd7819a7a33
Olivia <oliviatmanalastas@gmail.com>
2023-01-30 12:31:08 -0500

    Components levels

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,22 @@ import ShoppingList from './components/ShoppingList';
 import Greeter from './components/Greeter';
 
 function App() {
+     const items = [
+       {
+         id: 1,
+         product: "Lemon",
+         quantity: 3,
+       },
+       {
+         id: 2,
+         product: "Chicken Breast",
+         quantity: 2,
+       },
+     ];
+     
   return (
     <div className="App">
-      <ShoppingList />
+      <ShoppingList items={items}/>
     </div>
   );
 }
--- src/components/ShoppingList.tsx
+++ src/components/ShoppingList.tsx
@@ -1,23 +1,21 @@
 import React from "react";
 
-function ShoppingList(): JSX.Element {
-   const items = [
-      {
-         id: 1,
-         product: "Lemon",
-         quantity: 3,
-      },
-      {
-         id: 2,
-         product: "Chicken Breast",
-         quantity: 2
-      }
-   ]
+interface Item {
+   id: number;
+   product: string;
+   quantity: number
+}
+interface ShoppingListProps {
+   items: Item[]
+}
+function ShoppingList(props: ShoppingListProps): JSX.Element {
   return (
     <div>
       <h1> Shopping List</h1>
       <ul>
-        <li> Item 1</li>
+        {props.items.map(item => (
+         <li key={item.id}>{item.product} - {item.quantity}</li>
+        ))}
       </ul>
     </div>
   );
