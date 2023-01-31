499d594ab989cc2b1c0c32b3a039317e5ae60f4d
Olivia <oliviatmanalastas@gmail.com>
2023-01-31 09:47:01 -0500

    useRef in typescript

--- src/components/ShoppingListForm.tsx
+++ src/components/ShoppingListForm.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {useRef}from "react";
 
 export default function ShoppingListForm(): JSX.Element {
 
+   const inputRef = useRef<HTMLInputElement>(null)
    // const handleSubmit = (e : React.FormEvent) => {
    //    e.preventDefault()
    //    console.log("Submitted!!!")
@@ -14,7 +15,7 @@ export default function ShoppingListForm(): JSX.Element {
    return(
       <div>
          <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Product Name"/>
+            <input type="text" placeholder="Product Name" ref={inputRef}/>
             <button type="submit"> Add Product</button>
          </form>
       </div>
