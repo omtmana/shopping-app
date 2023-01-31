a85edef9bf6fd5a495a650bb6bd3253d407f795c
Olivia <oliviatmanalastas@gmail.com>
2023-01-31 10:03:06 -0500

    updating the state

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import ShoppingList from './components/ShoppingList';
 import Greeter from './components/Greeter';
 import ShoppingListForm from './components/ShoppingListForm';
 import Item from './models/item'
+import {v4 as getId, v4} from "uuid"
 import './App.css';
 
 
@@ -14,9 +15,11 @@ import './App.css';
 
 function App() {
    const [items, setItems] = useState<Item[]>([])
+
    const addItem = (product: string) => {
     console.log("Made to app component")
     console.log(product)
+    setItems([...items, {id: getId(), product, quantity: 1 }])
    }
     //  const items = [
     //    {
